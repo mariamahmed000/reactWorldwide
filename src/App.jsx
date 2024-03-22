dd93d57394c7de064c2a483f8c554b15c266b9a8
Sayed <sayedshehata33@gmail.com>
2024-03-22 20:34:56 +0200

    feat: :sparkles: Organize pages and add routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,28 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ThemeProvider, createTheme } from "@mui/material";
 import { themeSettings } from "./themes";
 import { useMemo } from "react";
-import HomePage from "./pages/HomePage";
+import HomePage from "./pages/homePage/HomePage";
+import ProfilePage from "./pages/profilePage/ProfilePage";
+import Login from "./pages/loginPage/Login";
+import Register from "./pages/regPage/Register";
+import ErrorPage from "./pages/errorPage/errorPage";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings("light")), []);
   return (
-    <>
+    <div className="app">
       <ThemeProvider theme={theme}>
         <BrowserRouter>
           <Routes>
-            <Route path="/home" element={<HomePage />}></Route>
+            <Route path="/" element={<HomePage />}></Route>
+            <Route path="/profile/:userId" element={<ProfilePage />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/reg" element={<Register />}></Route>
+            <Route path="*" element={<ErrorPage />}></Route>
           </Routes>
         </BrowserRouter>
       </ThemeProvider>
-    </>
+    </div>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
