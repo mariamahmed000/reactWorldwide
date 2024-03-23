e9a086e5eb1c0e46a0d9fbcaf83ca11be7742bb5
Sayed <sayedshehata33@gmail.com>
2024-03-23 03:58:33 +0200

    feat: :package: Initialize CDN for font-awesome

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,13 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+      integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+      crossOrigin="anonymous"
+      referrerPolicy="no-referrer"
+    />
     <App />
   </React.StrictMode>
 );
