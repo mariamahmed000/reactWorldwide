ad7233c70a748c15b61468363d3eb106f065e704
Sayed <sayedshehata33@gmail.com>
2024-03-23 03:56:53 +0200

    feat: :necktie: Add login and register

--- src/pages/loginPage/Login.jsx
+++ src/pages/loginPage/Login.jsx
@@ -1,5 +1,43 @@
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import Form from "./Form";
+
 const Login = () => {
-  return <div>Login page</div>;
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  const navigate = useNavigate();
+  return (
+    <div className="container">
+      <Box>
+        <Box
+          width="100%"
+          backgroundColor={theme.palette.background.alt}
+          p="1rem 6%"
+          textAlign="center"
+        >
+          <Typography
+            fontWeight="bold"
+            fontSize="32px"
+            color="primary"
+            onClick={() => navigate("/")}
+            sx={{ cursor: "pointer" }}
+          >
+            WorldWide
+          </Typography>
+        </Box>
+
+        <Box
+          width={isNonMobileScreens ? "50%" : "93%"}
+          p="2rem"
+          m="2rem auto"
+          borderRadius="1.5rem"
+          backgroundColor={theme.palette.background.alt}
+        >
+          <Form />
+        </Box>
+      </Box>
+    </div>
+  );
 };
 
 export default Login;
