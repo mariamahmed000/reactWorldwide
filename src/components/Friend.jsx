3f84f622a3d43646323947dcdf725633b5165a4e
Sayed <sayedshehata33@gmail.com>
2024-03-29 17:42:32 +0200

    feat: :sparkles: Make friendList dynamic

--- src/components/Friend.jsx
+++ src/components/Friend.jsx
@@ -1,13 +1,40 @@
-import { PersonRemoveOutlined } from "@mui/icons-material";
+import { PersonRemoveOutlined, PersonAddOutlined } from "@mui/icons-material";
 import { Box, IconButton, Typography, useTheme } from "@mui/material";
 import FlexBetween from "./utilities/FlexBetween";
 import UserImg from "./utilities/UserImg";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { setFriends } from "../redux/authSlice";
+import { useEffect } from "react";
 
-const Friend = () => {
-  const name = "Abdallah";
-  const subtitle = "Software Developer";
-  const userPicturePath =
-    "https://images.unsplash.com/photo-1503249023995-51b0f3778ccf?q=80&w=1920&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
+  // const name = "Abdallah";
+  // const subtitle = "Software Developer";
+  // const userPicturePath =
+  //   "https://images.unsplash.com/photo-1503249023995-51b0f3778ccf?q=80&w=1920&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { _id } = useSelector((state) => state.auth.user);
+  const token = useSelector((state) => state.auth.token);
+  const friends = useSelector((state) => state.auth.user.friends);
+
+  const isFriend = friends.find((friend) => friend._id === friendId);
+
+  const patchFriend = async () => {
+    const response = await fetch(
+      `http://localhost:7005/user/${_id}/${friendId}`,
+      {
+        method: "PUT",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    dispatch(setFriends({ friends: data }));
+  };
 
   const { palette } = useTheme();
   const primaryLight = palette.primary.light;
@@ -19,14 +46,19 @@ const Friend = () => {
     <FlexBetween>
       <FlexBetween gap="1rem">
         <UserImg image={userPicturePath} size="55px" />
-        <Box>
+        <Box
+          onClick={() => {
+            navigate(`/profile/${friendId}`);
+            navigate(0);
+          }}
+        >
           <Typography
             color={main}
             variant="h5"
             fontWeight="500"
             sx={{
               "&:hover": {
-                color: medium,
+                color: palette.primary.light,
                 cursor: "pointer",
               },
             }}
@@ -38,10 +70,43 @@ const Friend = () => {
           </Typography>
         </Box>
       </FlexBetween>
-      <IconButton sx={{ backgroundColor: primaryLight, p: "0.6rem" }}>
-        <PersonRemoveOutlined sx={{ color: primaryDark }} />
+      <IconButton
+        onClick={() => patchFriend()}
+        sx={{ backgroundColor: primaryLight, p: "0.6rem" }}
+      >
+        {isFriend ? (
+          <PersonRemoveOutlined sx={{ color: primaryDark }} />
+        ) : (
+          <PersonAddOutlined sx={{ color: primaryDark }} />
+        )}
       </IconButton>
     </FlexBetween>
+    // <FlexBetween>
+    //   <FlexBetween gap="1rem">
+    //     <UserImg image={userPicturePath} size="55px" />
+    //     <Box>
+    //       <Typography
+    //         color={main}
+    //         variant="h5"
+    //         fontWeight="500"
+    //         sx={{
+    //           "&:hover": {
+    //             color: medium,
+    //             cursor: "pointer",
+    //           },
+    //         }}
+    //       >
+    //         {name}
+    //       </Typography>
+    //       <Typography color={medium} fontSize="0.75rem">
+    //         {subtitle}
+    //       </Typography>
+    //     </Box>
+    //   </FlexBetween>
+    //   <IconButton sx={{ backgroundColor: primaryLight, p: "0.6rem" }}>
+    //     <PersonRemoveOutlined sx={{ color: primaryDark }} />
+    //   </IconButton>
+    // </FlexBetween>
   );
 };
 
--- src/components/FriendList.jsx
+++ src/components/FriendList.jsx
@@ -1,9 +1,37 @@
 import { Box, Typography, useTheme } from "@mui/material";
 import ComponentWrapper from "./utilities/ComponentWrapper";
 import Friend from "./Friend";
+import { useDispatch, useSelector } from "react-redux";
+import { setFriends } from "../redux/authSlice";
+import { useEffect } from "react";
 
-const FriendList = () => {
+const FriendList = ({ userId }) => {
+  const token = useSelector((state) => state.auth.token);
+  const currentUser = useSelector((state) => state.auth.user);
+  const isCurrentUser = userId === currentUser._id;
+  const userFriends = useSelector((state) => state.auth.user.friends);
+  console.log("USERFRIENDS", userFriends, "ISFRIEND", isCurrentUser);
+  const dispatch = useDispatch();
+
   const { palette } = useTheme();
+
+  const getFriends = async () => {
+    const response = await fetch(
+      `http://localhost:7005/user/${userId}/friends`,
+      {
+        method: "GET",
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+    const data = await response.json();
+    console.log("DATA", data);
+
+    dispatch(setFriends({ friends: data.data }));
+  };
+
+  useEffect(() => {
+    getFriends();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <ComponentWrapper>
@@ -16,9 +44,34 @@ const FriendList = () => {
         Friend List
       </Typography>
       <Box display="flex" flexDirection="column" gap="1.5rem">
-        <Friend />
+        {userFriends.length > 0 ? (
+          userFriends.map((friend) => (
+            <Friend
+              key={friend._id}
+              friendId={friend._id}
+              name={`${friend.firstName} ${friend.lastName}`}
+              subtitle={friend.occupation}
+              userPicturePath={friend.userImage}
+            />
+          ))
+        ) : (
+          <h1>no friends</h1>
+        )}
       </Box>
     </ComponentWrapper>
+    // <ComponentWrapper>
+    //   <Typography
+    //     color={palette.neutral.dark}
+    //     variant="h5"
+    //     fontWeight="500"
+    //     sx={{ mb: "1.5rem" }}
+    //   >
+    //     Friend List
+    //   </Typography>
+    //   <Box display="flex" flexDirection="column" gap="1.5rem">
+    //     <Friend />
+    //   </Box>
+    // </ComponentWrapper>
   );
 };
 
