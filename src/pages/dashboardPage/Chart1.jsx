fb0606fb8e6aad087292a9b776b3a2d227c42bc6
Sayed <sayedshehata33@gmail.com>
2024-04-01 13:56:15 +0200

    feat: :sparkles: Update Charts

--- src/pages/dashboardPage/Chart1.jsx
+++ src/pages/dashboardPage/Chart1.jsx
@@ -1,19 +1,17 @@
-
-import { BarChart } from '@mui/x-charts/BarChart';
-import { axisClasses } from '@mui/x-charts';
-import {useSelector } from 'react-redux';
-import { useEffect, useRef, useState } from 'react';
-
+import { BarChart } from "@mui/x-charts/BarChart";
+import { axisClasses } from "@mui/x-charts";
+import { useSelector } from "react-redux";
+import { useEffect, useRef, useState } from "react";
 
 const chartSetting = {
   yAxis: [
     {
-      label: 'Number of Users',
+      label: "Number of Users",
     },
   ],
   sx: {
     [`.${axisClasses.left} .${axisClasses.label}`]: {
-      transform: 'translate(-10px, 0)',
+      transform: "translate(-10px, 0)",
     },
   },
 };
@@ -22,26 +20,22 @@ export default function Chart2() {
   const usersArr = useSelector((state) => state.users.users.data);
   const chartContainerRef = useRef(null);
 
-
   const [chartSize, setChartSize] = useState({ width: 500, height: 500 });
 
   useEffect(() => {
     function handleResize() {
-      const newWidth = window.innerWidth * 0.6; 
-      const newHeight = window.innerHeight * 0.39; 
+      const newWidth = window.innerWidth * 0.6;
+      const newHeight = window.innerHeight * 0.39;
       setChartSize({ width: newWidth, height: newHeight });
     }
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
     handleResize();
 
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
-
-
-
 
   if (!usersArr) {
     return <div>Loading...</div>;
@@ -57,17 +51,17 @@ export default function Chart2() {
     location,
     count: locationCounts[location],
   }));
+  dataset.sort((a, b) => b.count - a.count);
 
   return (
     <div ref={chartContainerRef}>
       <BarChart
         dataset={dataset}
-        xAxis={[{ scaleType: 'band', dataKey: 'location' }]}
-        series={[{ dataKey: 'count', label: 'Number of Users' }]}
+        xAxis={[{ scaleType: "band", dataKey: "location" }]}
+        series={[{ dataKey: "count", label: "Number of Users" }]}
         width={chartSize.width}
         height={chartSize.height}
         {...chartSetting}
-  
       />
     </div>
   );
